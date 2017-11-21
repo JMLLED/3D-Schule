6f7a9acccf81e3d8684cc38b2a0f1535b783db94
jorolf <jorolf@gmx.de>
2017-11-21 22:11:14 +0100

    ersetze das alte Door skript durch ein modulareres

---
+++ Assets/Project/Interactables/Door.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework.Constraints;
+using UnityEngine;
+
+namespace Project.Interactables
+{
+    public class Door : MonoBehaviour, IInteractable
+    {
+        public float OpeningTime;
+        private bool open;
+        private Quaternion startingRotation;
+        private Quaternion openedRotation;
+        private float openingTime;
+
+
+
+        public void Interact()
+        {
+            if (openingTime > 0) return;
+
+            open = !open;
+            openingTime = OpeningTime;
+        }
+
+        private void Start()
+        {
+            startingRotation = transform.localRotation;
+            openedRotation = Quaternion.Euler(startingRotation.eulerAngles + new Vector3(0, 90, 0));
+        }
+
+        private void Update()
+        {
+            openingTime -= Time.deltaTime;
+            if (openingTime > 0)
+            {
+                transform.localRotation = open ? Quaternion.Slerp(openedRotation, startingRotation, openingTime / OpeningTime) : Quaternion.Slerp(startingRotation, openedRotation, openingTime / OpeningTime);
+            }
+        }
+    }
+}
---
+++ Assets/Project/Player/Interact.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Project.Player
+{
+    public class Interact : MonoBehaviour {
+
+        public Camera Camera1;
+
+        // Update is called once per frame
+        public void Update () {
+            if (Input.GetButtonDown("Interact"))
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(transform.position, Camera1.transform.forward, out hit)) {
+                    if(hit.distance <= 3)
+                    {
+                        hit.transform.GetComponent<IInteractable>().Interact();
+                    }
+                }
+
+            }
+        }
+    }
+}
