bcb1bfb879e89a1d2cbd86e1d0e46ff66e5c5a1b
jorolf <jorolf@gmx.de>
2017-11-28 22:02:04 +0100

    Skript zum entladen einer 2D Szene

---
+++ Assets/Project/UI/UnloadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Project.UI
+{
+    /// <summary>
+    /// Unloads its scene when interacted
+    /// </summary>
+    public class UnloadScene : MonoBehaviour
+    {
+        public void UnloadThisScene() => SceneManager.UnloadSceneAsync(gameObject.scene);
+    }
+}
