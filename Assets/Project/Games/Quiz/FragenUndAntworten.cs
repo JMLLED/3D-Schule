b8c02d928866b1fdcafecbbf5b524d839a9dd6cc
Unknown <maxi.gruendel@yahoo.de>
2017-12-19 14:06:50 +0100

    Anfang des Quizes

---
+++ Assets/Project/Games/Quiz/FragenUndAntworten.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace Project.Games.Quiz
+{
+    public class Frage
+    {
+
+        private string frage;
+        private bool antwort;
+
+        public Frage(string pFrage, bool pAntwort)
+        {
+            frage = pFrage;
+            antwort = pAntwort;
+        }
+
+        public string GetFrage()
+        {
+            return frage;
+        }
+
+        public bool GetAntwort()
+        {
+            return antwort;
+        }
+    }
+}
