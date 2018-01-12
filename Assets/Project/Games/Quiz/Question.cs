009aedc252f8da70da00f204eed9937d9e564801
Unknown <maxi.gruendel@yahoo.de>
2018-01-12 14:41:36 +0100

    Fortführung des Quizs
    Veränderung der Skripts. Quiz funktioniert schon.

---
+++ Assets/Project/Games/Quiz/Question.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Project.Games.Quiz
+{
+    [Serializable]
+    public class PolarQuestion
+    {
+        public string Question;
+        public bool Answer;
+    }
+}
---
+++ Assets/Project/Games/Quiz/Quiz.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Games.Quiz
+{
+    public class Quiz : MonoBehaviour
+    {
+        private PolarQuestion current;
+        public PolarQuestion[] Frage;
+        private  List<PolarQuestion> unanswered;
+        public Text Text;
+
+        private void RandomQuestion()
+        {
+            if (unanswered.Count != 0)
+            {
+                int temp = Random.Range(0, unanswered.Count);
+                current = unanswered[temp];
+                Text.text = current.Question;
+                unanswered.RemoveAt(temp);
+            }
+            else
+            {
+                current = null;
+            }
+        }
+
+        public void Start()
+        {
+            if (unanswered == null || unanswered.Count == 0)
+            {
+                unanswered = Frage.ToList();
+            }
+            RandomQuestion();
+        }
+
+        public void True()
+        {
+            if (current != null)
+            {
+                Debug.Log(current.Answer ? "Richtig" : "Falsch");
+                RandomQuestion();
+            }
+            else
+            {
+                Debug.Log("Keine Fragen mehr!");
+            }
+        }
+
+        public void False()
+        {
+            if (current != null)
+            {
+                Debug.Log(current.Answer ? "Falsch" : "Richtig");
+                RandomQuestion();
+            }
+            else
+            {
+                Debug.Log("Keine Fragen mehr!");
+            }
+        }
+
+
+    }
+}
