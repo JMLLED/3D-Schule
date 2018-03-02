66df3f3df6d7c55b187b192abde7b02b43e61fe9
jorolf <jorolf@gmx.de>
2018-03-02 14:46:31 +0100

    erste anfänge

---
+++ Assets/Project/Games/Indicator/Indicator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Project.Games.Indicator
+{
+    public class Indicator : MonoBehaviour
+    {
+        public void Update ()
+        {
+            double ph = GetPHValue();
+            Material material = transform.GetComponent();
+            if(ph < 3) material.color = Color.red;
+            else if(ph < 6) material.color = Color.yellow;
+            else if(ph < 8) material.color = Color.green;
+            else if(ph < 11) material.color = Color.blue;
+            else material.color = Color.magenta;
+        }
+
+        private double GetPHValue() => 7;
+    }
+}
